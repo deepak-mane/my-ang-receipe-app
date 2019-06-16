c1d785ca1988839276fa07f710c7c3f6b6f467ed
Deepak Mane <deepak.mane123@gmail.com>
2019-06-16 01:03:18 -0700

    Wrap Up completed recipe

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+import { stringify } from "querystring";
+
+export class Ingredient {
+    // public  name: string;
+    // public  amount: number;
+
+    constructor(public name: string, public amount: number) {
+        // this.name = name;
+        // this.amount = amount;
+
+    }
+
+}
