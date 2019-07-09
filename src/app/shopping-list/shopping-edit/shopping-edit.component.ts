288db327a4e0f97ce6e7c1e77470f1b9d9c2c325
Deepak Mane <deepak.mane123@gmail.com>
2019-07-09 05:08:47 -0700

    218. TD:Allowing the Selection of Items in the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,24 +1,39 @@
 // GotRid - import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 // import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit ,
+  OnDestroy
+} from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ShoppingListService } from '../shopping-list.service';
+
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   // @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
   // @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
   // GotRid - @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+        }
+      );
   }
 
   onAddItem(form: NgForm) {
@@ -30,5 +45,9 @@ export class ShoppingEditComponent implements OnInit {
     // GotRid - this.ingredientAdded.emit(newIngredient);
     // Added Below now -
     this.slService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
